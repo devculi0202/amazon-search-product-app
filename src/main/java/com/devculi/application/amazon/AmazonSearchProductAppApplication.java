b8b4f6ce1e05ecbee9b172db63da65dc23e127db
devculi <lethaiduy0202@gmail.com>
2024-01-21 22:39:44 +0700

    init project

---
+++ src/main/java/com/devculi/application/amazon/AmazonSearchProductAppApplication.java
@@ -0,0 +1,13 @@
+package com.devculi.application.amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonSearchProductAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AmazonSearchProductAppApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/devculi/application/amazon/AmazonSearchProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devculi.application.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonSearchProductAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
